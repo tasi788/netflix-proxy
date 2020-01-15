dc1c1c3a7749399b5c31e1b433bb4545be850cc8
tdc <tdc@tdccc.com.tw>
2020-01-15 15:39:56 +0800

    set sniproxy

--- docker-sniproxy/run.sh
+++ docker-sniproxy/run.sh
@@ -15,4 +15,15 @@
 
 # showip
 # openvpn --config /openvpn/default.ovpn
+
+# set environment from linked container information
+RESOLVER_IP=$(grep caching-resolver /etc/hosts |  head -n 1 | awk '{print $1}')
+
+if [ -z ${RESOLVER_IP} ]; then
+    RESOLVER_IP=8.8.8.8
+fi
+
+# update sniproxy config
+printf "Setting sniproxy resolver to ${RESOLVER_IP}\n"
+sed -i -r "s/nameserver ([0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3})/nameserver ${RESOLVER_IP}/" /etc/sniproxy.conf
 $(which sniproxy) -c /etc/sniproxy.conf -f
